7576b98c3a7fce08423192e63ee417a0b3bfac32
Cloé <cloe.ricard@efrei.net>
2021-11-24 18:14:45 +0100

    Last version of the lab (realizing only today that the git was not up to date... sorry for that)

--- db_connection.js
+++ db_connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
+    user: "root",
     password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE
 });
